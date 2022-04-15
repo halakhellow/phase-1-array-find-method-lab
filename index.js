8d74406ce9a8baef49b091298e848918a77575c7
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-16 02:28:51 +0300

    define superbowlWin function which use find method to check for a win in a set of records

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+let superbowlWin =  (records) => {
+    let searchResult = records.find(record => record.result==="W");
+    return searchResult && searchResult.year
+}
+
